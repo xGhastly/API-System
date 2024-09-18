1ff3eec018ee6035148b39ff234f2e4a8f3e27e6
João Augusto <joaoaugustozm@hotmail.com>
2024-09-18 11:45:10 -0300

    att

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTIONSTRING)
   .then(() => {
     app.emit('pronto');
   })
